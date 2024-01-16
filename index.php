90fec092456998b246a039a4d49387a8888b87e5
kristianfiliposki <krisplay@outlook.it>
2024-01-16 23:06:19 +0100

    construct

--- index.php
+++ index.php
@@ -2,12 +2,16 @@
 
     require __DIR__ . '/model/movies.php';
 
-    $shrek= new Movie();
-    $shrek->name="ritoieno al fututoe";
+    $shrek= new Movie("xiao","ok","ce");
+/*     $shrek->name="ritoieno al fututoe";
     $shrek->language="ing";
-    $shrek->movieMaker="speke lee";
+    $shrek->movieMaker="speke lee"; */ 
 
 
 /*     var_dump($shrek);
- */    
+ */ 
+
     echo $shrek->cliper();
+ 
+
+
--- model/movies.php
+++ model/movies.php
@@ -4,6 +4,12 @@
       public $name;        
       public $language;
       public $movieMaker;
+
+      public function __construct($nome,$lingua,$regista){
+            $this->name = $nome;
+            $this->language = $lingua;
+            $this->movieMaker = $regista;
+      }
       
       public function cliper (){
           $frase =  $this->name . ", " . $this->language . "," . $this->movieMaker ;
