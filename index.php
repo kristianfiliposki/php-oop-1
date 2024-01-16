7d1fad1d9cf0941c0e5a926be5cb7c9f67b2a6f6
kristianfiliposki <krisplay@outlook.it>
2024-01-16 22:51:21 +0100

    function + frase + variabili

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php 
 
-    require __DIR__ . '/model/movie.php';
+    require __DIR__ . '/model/movies.php';
 
-    $shrek = new movie();
-    $shrek->$name = "ciao";   
-    $shrek->$filmMaker = "ciao";
-    $shrek->$language = "ciao";   
+    $shrek= new Movie();
+    $shrek->name="ritoieno al fututoe";
+    $shrek->language="ing";
+    $shrek->movieMaker="speke lee";
 
-    var_dump($shrek);
+
+/*     var_dump($shrek);
+ */    
+    echo $shrek->cliper();
--- model/movies.php
+++ model/movies.php
@@ -1,37 +1,12 @@
 
-<?php
-class movie{
-
-    public $name;
-    public $author;
-    public $language;
-
-     //Costruttore, viene richiamato dove scriviamo new movie(...)
-    public function __construct($nome,$lingua,$regista){
-        $this->$name=$nome;
-        $this->$filmMaker=$regista;
-        $this->$language=$lingua;
+<?php 
+    class Movie{
+      public $name;        
+      public $language;
+      public $movieMaker;
+      
+      public function cliper (){
+          $frase =  $this->name . ", " . $this->language . "," . $this->movieMaker ;
+          return $frase; 
+        }
     }
-
-}
-/* class Address
-{
-    public $street;
-    public $number;
-    public $postalCode;
-    public $city;
-    public $country;
-
-    //Costruttore, viene richiamato dove scriviamo new Address(...)
-    public function __construct($strada, $civico, $cap, $citta, $nazione)
-    {
-        //Prendo gli argomenti passati e li assegno a QUESTA istanza che sto creando
-        $this->street = $strada;
-        $this->number = $civico;
-        $this->postalCode = $cap;
-        $this->city = $citta;
-        $this->country = $nazione;
-    }
-
-
-} */
